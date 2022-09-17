5bb940003b3507b58b6819912f496849c55f6774
Mustafa <mustafa14412@gmail.com>
2022-09-17 16:48:21 +0300

    second

---
+++ app.js
@@ -0,0 +1,9 @@
+//selectors 
+const eksi=document.querySelector("#eksi")
+const artı=document.querySelector("#artı")
+const p1=document.querySelector("#p1")
+console.log(p1);
+
+eksi.addEventListener("click",()=>{
+  
+})
